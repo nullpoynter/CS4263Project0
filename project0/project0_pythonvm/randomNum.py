ad11106313ef679e9d858270ced6863e180b65dc
Nathanv97 <43190392+Nathanv97@users.noreply.github.com>
2019-09-12 18:34:46 -0500

    Updated randomNum.py

--- project0/project0_pythonvm/randomNum.py
+++ project0/project0_pythonvm/randomNum.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-	return str(random.randint(1,1000001))
+	return str(random.randint(1,1000001)) #upper bound is exclusive so, make it 1000001
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=80)
+	app.run(host='0.0.0.0', port=80) #port to send and receive data
